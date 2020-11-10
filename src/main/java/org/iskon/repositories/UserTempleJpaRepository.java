885523fc71a047791691e1193632e0a8cce987b8
Rahul <Rahul@MSI>
2020-11-10 21:29:08 +0530

    Fixed UserTemple Error

--- src/main/java/org/iskon/repositories/UserTempleJpaRepository.java
+++ src/main/java/org/iskon/repositories/UserTempleJpaRepository.java
@@ -3,7 +3,7 @@ package org.iskon.repositories;
 import java.util.List;
 
 import org.iskon.models.UserTemple;
-import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,7 +12,8 @@ import org.springframework.stereotype.Repository;
 public interface UserTempleJpaRepository extends JpaRepository<UserTemple, Integer> {
 	List<UserTemple> findAll(Specification<UserTemple> eventSpecification);
 
-	@Query(" SELECT new org.iskon.models.UserTemple(UT.id, UT.templeId, UT.roleId, UT.userId, T.templeName, U.userName) "
+	@Query("SELECT new org.iskon.models.UserTemple(UT.id, UT.templeId, UT.roleId, UT.userId, T.templeName, U.userName) "
 			+ " FROM UserTemple UT, Temple T, User U " + " where UT.templeId = T.id " + " and UT.userId = U.id ")
 	List<UserTemple> findAllTwo();
+	
 }
