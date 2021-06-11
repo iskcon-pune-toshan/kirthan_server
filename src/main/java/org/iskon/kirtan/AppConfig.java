cecdcde50cea33c1e1edf5f2448aaf5aad11402f
Afrah Shaikh <afrah.17u278@viit.ac.in>
2021-06-11 22:43:41 +0530

    Application.properties for different environment

--- src/main/java/org/iskon/kirtan/AppConfig.java
+++ src/main/java/org/iskon/kirtan/AppConfig.java
@@ -1,23 +1,40 @@
 package org.iskon.kirtan;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @ComponentScan("org.iskon")
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 
+	@Value("${spring.datasource.url}")
+    private String url;
+	
+	@Value("${spring.datasource.username}")
+    private String username;
+	
+	@Value("${spring.datasource.password}")
+    private String password; 
+	
+	@Value("${spring.datasource.driver-class-name}")
+    private String driverclassname; 
+	
 	@Bean(name="dataSource")
 	public DriverManagerDataSource getDriverManagerSource()
 	{
 		DriverManagerDataSource dmds = new DriverManagerDataSource();
-		dmds.setDriverClassName("com.mysql.jdbc.Driver");
-		dmds.setUrl("jdbc:mysql://localhost:3306/kirtan_stg");
-		dmds.setUsername("root");
-		dmds.setPassword("root");
+		dmds.setDriverClassName(driverclassname);
+		dmds.setUrl(url);
+		dmds.setUsername(username);
+		dmds.setPassword(password);
 		return dmds;
 	}
 	
